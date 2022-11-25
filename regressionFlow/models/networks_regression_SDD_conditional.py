2d1f4fee57e485d3d0c3f52cc182128bb1b0fb25
she3r <piotr.kubacki@student.uj.edu.pl>
2022-11-25 17:31:36 +0100

    add zeros warmup

--- regressionFlow/models/networks_regression_SDD_conditional.py
+++ regressionFlow/models/networks_regression_SDD_conditional.py
@@ -11,31 +11,34 @@ from torch.distributions.laplace import Laplace
 
 def conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1, padding=None):
     if padding is None:
-        padding_inside = (kernel_size-1)//2
+        padding_inside = (kernel_size - 1) // 2
     else:
         padding_inside = padding
     if batchNorm:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding_inside, bias=False),
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding_inside,
+                      bias=False),
             nn.BatchNorm2d(out_planes),
             nn.LeakyReLU(0.1, inplace=True)
         )
     else:
         return nn.Sequential(
             nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding_inside, bias=True),
-            nn.LeakyReLU(0.1,inplace=True)
+            nn.LeakyReLU(0.1, inplace=True)
         )
 
 
-def i_conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1, bias = True):
+def i_conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1, bias=True):
     if batchNorm:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=bias),
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size - 1) // 2,
+                      bias=bias),
             nn.BatchNorm2d(out_planes),
         )
     else:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=bias),
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size - 1) // 2,
+                      bias=bias),
         )
 
 
@@ -46,7 +49,7 @@ def predict_flow(in_planes):
 def deconv(in_planes, out_planes):
     return nn.Sequential(
         nn.ConvTranspose2d(in_planes, out_planes, kernel_size=4, stride=2, padding=1, bias=True),
-        nn.LeakyReLU(0.1,inplace=True)
+        nn.LeakyReLU(0.1, inplace=True)
     )
 
 
@@ -56,6 +59,7 @@ class tofp16(nn.Module):
 
     def forward(self, input):
         return input.half()
+
 
 class tofp32(nn.Module):
     def __init__(self):
@@ -68,7 +72,7 @@ class tofp32(nn.Module):
 def init_deconv_bilinear(weight):
     f_shape = weight.size()
     heigh, width = f_shape[-2], f_shape[-1]
-    f = np.ceil(width/2.0)
+    f = np.ceil(width / 2.0)
     c = (2 * f - 1 - f % 2) / (2.0 * f)
     bilinear = np.zeros([heigh, width])
     for x in range(width):
@@ -78,7 +82,7 @@ def init_deconv_bilinear(weight):
     weight.data.fill_(0.)
     for i in range(f_shape[0]):
         for j in range(f_shape[1]):
-            weight.data[i,j,:,:] = torch.from_numpy(bilinear)
+            weight.data[i, j, :, :] = torch.from_numpy(bilinear)
 
 
 class ListModule(nn.Module):
@@ -108,12 +112,22 @@ class CRegression(nn.Module):
     def __init__(self, args):
         super(CRegression, self).__init__()
         self.input_dim = args.input_dim
-        self.flownet = FlowNetS()   # TODO zadbac zeby flownet zwracal to co definiuje z_dim
+        self.flownet = FlowNetS() 
         self.args = args
         self.point_cnf = get_point_cnf(args)
         self.gpu = args.gpu
         self.logprob_type = args.logprob_type
-        self.const_sample = self.sample_gaussian((5,65,1), None, self.gpu)
+        self.const_sample = self.sample_gaussian((5, 65, 1), None, self.gpu)
+
+        self.data_loader_len = 100  # rozmiar batcha (treningowego)
+        self.curr_epoch = 0
+        self.num_zeros_warmup_epochs = 10
+        self.num_gauss_warmup_epochs = 50
+
+        self.value_step = 1 / self.num_gauss_warmup_epochs
+        self.curr_batch_progress = 0
+        self.sample_w = 0
+
     def make_optimizer(self, args):
         def _get_opt_(params):
             if args.optimizer == 'adam':
@@ -124,31 +138,33 @@ class CRegression(nn.Module):
             else:
                 assert 0, "args.optimizer should be either 'adam' or 'sgd'"
             return optimizer
+
         opt = _get_opt_(list(self.flownet.parameters()) + list(self.point_cnf.parameters()))
         return opt
 
-    def forward(self, x: torch.tensor):    # x.shape = 5,65 = 5,64 + bias
+    def forward(self, x: torch.tensor):  # x.shape = 5,65 = 5,64 + bias
+        if self.curr_batch_progress == self.data_loader_len:
+            self.curr_epoch += 1
+            self.curr_batch_progress = 0
+            if self.curr_epoch >= self.num_zeros_warmup_epochs:
+                self.sample_w += self.value_step
+
+        self.curr_batch_progress += 1
         batch_size = x.size(0)  # 5
-        tn_num_values = x.size(1)   # 65
+        tn_num_values = x.size(1)  # 65
 
         # 1) output z hypernetworka zamieniamy na embedding rozmiaru z_dim
-
-        # podejscie 1: omijamy flownet, bo tam sa jakies konwolucje. Zakladamy z_dim=65*5 i wrzucamy flattena do point_cnf
         # z = self.flownet(x)
         z = x.flatten()
 
         # 2) wylosuj sample z rozkladu normalnego (chyba powinien byc w ksztalcie pozadanego outputu).
-        # todo trzeba wybadac co robi to input_dim dokladnie
-        # my bierzemy domyslnie Normal, mozna zawsze zmienic
-        if self.logprob_type == "Laplace":
-            y = self.sample_laplace((*x.shape, self.input_dim), self.gpu)
-        if self.logprob_type == "Normal":
-            # y = self.sample_gaussian((*x.shape, self.input_dim), None, self.gpu)
-            # y = self.sample_gaussian((5,65,1), None, self.gpu)
-            # y = torch.zeros(5,65,1).cuda(self.gpu)
-            y = self.const_sample
+        if self.curr_epoch >= self.num_zeros_warmup_epochs:
+            y = self.sample_w * self.sample_gaussian((1, 1, batch_size * tn_num_values), None, self.gpu)
+        else:
+            y = torch.zeros(1, 1, batch_size * tn_num_values).cuda(self.gpu)
+
         # 3) przerzuc przez flow -> w_i := F_{\theta}(z_i) tu z: embedding rozmiaru 100
-        target_networks_weights = self.point_cnf(y.reshape(1,1,-1), z.reshape(1,-1), reverse=True).view(*y.size())
+        target_networks_weights = self.point_cnf(y, z.reshape(1, -1), reverse=True).view(*y.size())
 
         # ------- LOSS ----------
         # # Liczenie DKL. Pytanie- na spotkaniu co rozumielismy przez batche? W konktescie TN to batchem
@@ -172,30 +188,7 @@ class CRegression(nn.Module):
         # loss = loss - loss_density
         loss = torch.tensor([0])
 
-        return target_networks_weights.reshape(5,65), loss
-
-
-    # def forward(self, x, y, opt, step, writer=None):
-    #     opt.zero_grad()
-    #     batch_size = x.size(0)
-    #     z = self.flownet(x)
-    #
-    #     # Loss
-    #     y, delta_log_py = self.point_cnf(y, z, torch.zeros(batch_size, y.size(1), 1).to(y))
-    #     if self.logprob_type == "Laplace":
-    #         log_py = standard_laplace_logprob(y).view(batch_size, -1).sum(1, keepdim=True)
-    #     if self.logprob_type == "Normal":
-    #         log_py = standard_normal_logprob(y).view(batch_size, -1).sum(1, keepdim=True)
-    #     delta_log_py = delta_log_py.view(batch_size, y.size(1), 1).sum(1)
-    #     log_px = log_py - delta_log_py
-    #
-    #     loss = -log_px.mean()
-    #
-    #     loss.backward()
-    #     opt.step()
-    #     recon = -log_px.sum()
-    #     recon_nats = recon / float(y.size(0))
-    #     return recon_nats
+        return target_networks_weights.reshape(5, 65), loss
 
     @staticmethod
     def sample_gaussian(size, truncate_std=None, gpu=None):
@@ -211,7 +204,6 @@ class CRegression(nn.Module):
         y = m.sample(sample_shape=torch.Size([size[0], size[1], size[2]])).float().squeeze(3)
         y = y if gpu is None else y.cuda(gpu)
         return y
-
 
     def decode(self, z, num_points):
         # transform points from the prior to a point cloud, conditioned on a shape code
@@ -260,7 +252,7 @@ class FlowNetS(nn.Module):
         self.conv6_1 = conv(self.batchNorm, 1024, 1024)
         self.conv7 = conv(self.batchNorm, 1024, 1024, kernel_size=1, stride=1, padding=0)
         self.conv8 = conv(self.batchNorm, 1024, 1024, kernel_size=1, stride=1, padding=0)
-        #self.fc1 = nn.Linear(in_features=46080, out_features=1024, bias=True)
+        # self.fc1 = nn.Linear(in_features=46080, out_features=1024, bias=True)
         self.fc1 = nn.Linear(in_features=46080, out_features=1024, bias=True)
         self.fc2 = nn.Linear(in_features=1024, out_features=1024, bias=True)
         # self.predict_6 = predict_flow(1024)
@@ -298,5 +290,5 @@ class FlowNetS(nn.Module):
         # out_fc2 = self.fc1(out_conv8.view(out_conv8.size(0), -1))
         out_fc1 = nn.functional.relu(self.fc1(out_conv8.view(out_conv6.size(0), -1)))
         out_fc2 = nn.functional.relu(self.fc2(out_fc1))
-        #out_fc2 = self.predict_6(out_conv6)
+        # out_fc2 = self.predict_6(out_conv6)
         return out_fc2
